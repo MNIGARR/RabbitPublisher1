e0cf6d120978fd3bdd4507d3b71fada59b751550
Musta_cz12 <Musta_cz12@STHQ0118-15>
2024-05-13 09:51:03 +0400

    Add project files.

---
+++ RabbitConsumer1/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitConsumer1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://hkqqhlch:b5wm2yEAKLpohoWau42wJvRQNWOBORWt@chimpanzee.rmq.cloudamqp.com/hkqqhlch");
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+            channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+            var queueName = channel.QueueDeclare().QueueName;
+            channel.QueueBind(queue: queueName,
+                exchange: "logs",
+                routingKey: string.Empty);
+
+            var consumer = new EventingBasicConsumer(channel);  
+            channel.BasicConsume(queueName, true, consumer);
+            consumer.Received += Consumer_Received;
+
+            Console.ReadLine();
+
+        }
+
+
+        private static void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+        {
+            var message = Encoding.UTF8.GetString(e.Body.ToArray());
+            Console.WriteLine($"Received message : {message}");
+
+        }
+    }
+}
---
+++ RabbitPublisher1/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitPublisher1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://psqyqkas:QYmaSp0TjmkU91ADQ-m0oc8AZFob-vhj@puffin.rmq2.cloudamqp.com/psqyqkas");
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(exchange: "dirext_logs", type: ExchangeType.Direct);
+            var message = "Hello Worl";
+            while (true)
+            {
+                Console.WriteLine("Warning or Error or Info");
+                var severity = Console.ReadLine();
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange:"direct_logs",
+                    routingKey: severity,
+                    basicProperties: null
+               
+            }
+
+        }
+    }
+}
